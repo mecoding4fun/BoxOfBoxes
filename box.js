698ff9dca918ed7f3a38cb004421ba8da726169e
mecoding4fun <53414760+mecoding4fun@users.noreply.github.com>
2020-03-11 14:46:46 +0530

    Update box.js

--- box.js
+++ box.js
@@ -4,15 +4,20 @@ class Box{
         this.width = width;
         this.height = height;
         World.add(world, this.body);
+        this.image = loadImage("01.png");
+
     }
     display(){
         var pos = this.body.position;
         var angle = this.body.angle;
+        fill(255,0,0);
         push();
         translate(pos.x, pos.y);
         rotate(angle);
         rectMode(CENTER);
         rect(0, 0, this.width, this.height);
+        imageMode(CENTER);
+        image(this.image, 0, 0, 100, 100);
         pop();
     }
 }
